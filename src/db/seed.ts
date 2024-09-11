7871e0a6bb15c9d3e4dec098cf3a4675952f8a9d
SulivanGalahad <sulivanpm@gmail.com>
2024-09-11 20:07:38 -0300

    routes

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,15 +1,26 @@
-import { db } from '.'
+import { client, db } from '.'
 import { goalCompletions, goals } from './schema'
+import dayjs from 'dayjs'
 
 async function seed() {
   await db.delete(goalCompletions)
   await db.delete(goals)
 
-  await db.insert(goals).values([
-    { title: 'Reagar plantas', desiredWeeklyFrequency: 7 },
-    { title: 'Treinar', desiredWeeklyFrequency: 5 },
-    { title: 'Estudar React', desiredWeeklyFrequency: 5 },
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: 'Reagar plantas', desiredWeeklyFrequency: 7 },
+      { title: 'Treinar', desiredWeeklyFrequency: 5 },
+      { title: 'Estudar React', desiredWeeklyFrequency: 5 },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.insert(goalCompletions).values([
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
   ])
 }
 
-seed()
+seed().finally(() => client.end())
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,29 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+//import { request } from "http";
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
+//import { title } from "process";
 
 const app = fastify()
 
-app.listen({
-    port: 3333,
-}).then(() => {
-    console.log('HTTP server running!')
+app.post('/goals', async request => {
+  const createGoalSchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  })
+
+  const body = createGoalSchema.parse(request.body)
+
+  await createGoal({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  })
 })
 
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
